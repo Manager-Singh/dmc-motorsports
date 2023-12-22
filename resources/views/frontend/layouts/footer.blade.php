28dce2aa8e43c7da1d10aafe9bbd216e51ed8df1
Pankaj Kumar <managersingh1991@gmail.com>
2023-12-22 08:03:40 +0530

    resource update

--- resources/views/frontend/layouts/footer.blade.php
+++ resources/views/frontend/layouts/footer.blade.php
@@ -90,40 +90,40 @@
 	<!-- /End Footer Area -->
  
 	<!-- Jquery -->
-    <script src="{{asset('public/ffrontend/js/jquery.min.js')}}"></script>
-    <script src="{{asset('public/ffrontend/js/jquery-migrate-3.0.0.js')}}"></script>
-	<script src="{{asset('public/ffrontend/js/jquery-ui.min.js')}}"></script>
+    <script src="{{asset('public/frontend/js/jquery.min.js')}}"></script>
+    <script src="{{asset('public/frontend/js/jquery-migrate-3.0.0.js')}}"></script>
+	<script src="{{asset('public/frontend/js/jquery-ui.min.js')}}"></script>
 	<!-- Popper JS -->
-	<script src="{{asset('public/ffrontend/js/popper.min.js')}}"></script>
+	<script src="{{asset('public/frontend/js/popper.min.js')}}"></script>
 	<!-- Bootstrap JS -->
-	<script src="{{asset('public/ffrontend/js/bootstrap.min.js')}}"></script>
+	<script src="{{asset('public/frontend/js/bootstrap.min.js')}}"></script>
 	<!-- Color JS -->
-	<script src="{{asset('public/ffrontend/js/colors.js')}}"></script>
+	<script src="{{asset('public/frontend/js/colors.js')}}"></script>
 	<!-- Slicknav JS -->
-	<script src="{{asset('public/ffrontend/js/slicknav.min.js')}}"></script>
+	<script src="{{asset('public/frontend/js/slicknav.min.js')}}"></script>
 	<!-- Owl Carousel JS -->
-	<script src="{{asset('public/ffrontend/js/owl-carousel.js')}}"></script>
+	<script src="{{asset('public/frontend/js/owl-carousel.js')}}"></script>
 	<!-- Magnific Popup JS -->
-	<script src="{{asset('public/ffrontend/js/magnific-popup.js')}}"></script>
+	<script src="{{asset('public/frontend/js/magnific-popup.js')}}"></script>
 	<!-- Waypoints JS -->
-	<script src="{{asset('public/ffrontend/js/waypoints.min.js')}}"></script>
+	<script src="{{asset('public/frontend/js/waypoints.min.js')}}"></script>
 	<!-- Countdown JS -->
-	<script src="{{asset('public/ffrontend/js/finalcountdown.min.js')}}"></script>
+	<script src="{{asset('public/frontend/js/finalcountdown.min.js')}}"></script>
 	<!-- Nice Select JS -->
-	<script src="{{asset('public/ffrontend/js/nicesellect.js')}}"></script>
+	<script src="{{asset('public/frontend/js/nicesellect.js')}}"></script>
 	<!-- Flex Slider JS -->
 	<script src="{{asset('public/frontend/js/flex-slider.js')}}"></script>
 	<!-- ScrollUp JS -->
-	<script src="{{asset('public/ffrontend/js/scrollup.js')}}"></script>
+	<script src="{{asset('public/frontend/js/scrollup.js')}}"></script>
 	<!-- Onepage Nav JS -->
-	<script src="{{asset('public/ffrontend/js/onepage-nav.min.js')}}"></script>
+	<script src="{{asset('public/frontend/js/onepage-nav.min.js')}}"></script>
 	{{-- Isotope --}}
-	<script src="{{asset('public/ffrontend/js/isotope/isotope.pkgd.min.js')}}"></script>
+	<script src="{{asset('public/frontend/js/isotope/isotope.pkgd.min.js')}}"></script>
 	<!-- Easing JS -->
-	<script src="{{asset('public/ffrontend/js/easing.js')}}"></script>
+	<script src="{{asset('public/frontend/js/easing.js')}}"></script>
 
 	<!-- Active JS -->
-	<script src="{{asset('public/ffrontend/js/active.js')}}"></script>
+	<script src="{{asset('public/frontend/js/active.js')}}"></script>
 
 	
 	@stack('scripts')
